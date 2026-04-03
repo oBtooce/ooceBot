8a15a8fadd347bdc95b16531fb305ffe5215430f
alexwaddell <alex.b.waddell@gmail.com>
2026-04-02 23:36:39 -0400

    100-bit reward now has more effects!

--- Miscellaneous/TestMethod.cs
+++ Miscellaneous/TestMethod.cs
@@ -14,23 +14,38 @@ namespace ooceBot.Miscellaneous
     {
         public static async Task TestStuff()
         {
-            // Ideas: SM64 star get soundbyte, confetti overlay, 
             OBSWebsocket websocket = await OBSManager.ConnectToOBSWebsocket();
 
-            // The scene needs to exist in the currently selected scene, so fetch the current scene name and its items
+            // The source needs to exist in the currently selected scene, so fetch the current scene name and its items
             var currentScene = websocket.GetCurrentProgramScene();
             var sceneItems = websocket.GetSceneItemList(currentScene);
 
-            // Need to figure out a way to make the source name not a string because this is a bad setup
-            var goldStarScene = sceneItems.First(item => item.SourceName == "Gold Star");
+            var thanksScene = sceneItems.First(item => item.SourceName == "Thanks");
 
-            websocket.SetSceneItemEnabled(currentScene, goldStarScene.ItemId, true);
-
+            websocket.SetSceneItemEnabled(currentScene, thanksScene.ItemId, true);
             PlaySounds.PlaySound($"{ConfigurationManager.AppSettings["SoundsFolder"]}\\Super_Mario_64_Star_Get_Sound_Effect.mp3");
 
-            await Task.Delay(5000);
+            await Task.Delay(500);
 
+            PlaySounds.PlaySound($"{ConfigurationManager.AppSettings["SoundsFolder"]}\\SoundEffects\\Here_You_Go.mp3");
+
+            await Task.Delay(1000);
+
+            PlaySounds.PlaySound($"{ConfigurationManager.AppSettings["SoundsFolder"]}\\SoundEffects\\Reverb_Fart.mp3");
+
+            await Task.Delay(600);
+
+            var goldStarScene = sceneItems.First(item => item.SourceName == "Gold Star");
+            var confettiScene = sceneItems.First(item => item.SourceName == "Confetti");
+
+            websocket.SetSceneItemEnabled(currentScene, goldStarScene.ItemId, true);
+            websocket.SetSceneItemEnabled(currentScene, confettiScene.ItemId, true);
+
+            await Task.Delay(4100);
+
+            websocket.SetSceneItemEnabled(currentScene, thanksScene.ItemId, false);
             websocket.SetSceneItemEnabled(currentScene, goldStarScene.ItemId, false);
+            websocket.SetSceneItemEnabled(currentScene, confettiScene.ItemId, false);
         }
     }
 }
